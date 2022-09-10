87f9e61f4836017a80aa4559855279d66c6dab34
Felipe Andrés Alvarez Avaria <alu0100969535@ull.edu.es>
2022-09-10 20:25:10 +0100

    Initial commit

---
+++ Assets/Scripts/Tank/TankCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TankCamera : MonoBehaviour {
+
+	[SerializeField] private Transform cameraPivot;
+
+	public void SetCamera(Transform camera) {
+		camera.parent = this.cameraPivot;
+		camera.localEulerAngles = Vector3.zero;
+		camera.localPosition = Vector3.zero;
+	}
+
+}
